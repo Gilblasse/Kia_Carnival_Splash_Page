c7bf45e235ffcfd05d14e9ab070de83995270ec9
JasminDe <jblasse95@gmail.com>
2023-09-05 19:48:55 -0400

    added titles/discriptions 4 Features

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Features from './components/Features';
+import featureDiscrip from './copy/feature';
 
 function App() {
  return(
@@ -9,7 +10,11 @@ function App() {
     <div className='leftSide'>
       <h1 className='leftSideHeader'>Connectivity at every turn.</h1>
       <div className='vehicleFeaturesList'>
-        {/* <Features /> */}
+        <Features title="Remote Start w/ Climate Control"  discription={featureDiscrip.feature1}/>
+        <Features title="Find My Car"  discription={featureDiscrip.feature2}/>
+        <Features title="Voice Assistants"  discription={featureDiscrip.feature3}/>
+        <Features title="Connected Routing"  discription={featureDiscrip.feature4}/>
+        <Features title="Wi-Fi Hotspot"  discription={featureDiscrip.feature5}/>
       </div>
       <button className='button'>up</button>
       <button className='button'>Down</button>
