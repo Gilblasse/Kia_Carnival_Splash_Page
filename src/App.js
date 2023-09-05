a2022596a8c5be323979ecfd3e74dd0112617a09
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-09-05 00:46:34 +0000

    added features component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Features from "./components/Features";
 
 function App() {
  return(
   <div className='main-container'>
     <div className='leftSide'>
       <h1 className='leftSideHeader'>Connectivity at every turn.</h1>
-      <div className='vehicleFeaturesList'></div>
+      <div className='vehicleFeaturesList'>
+        {/* <Features/> */}
+      </div>
       <button className='button'>up</button>
       <button className='button'>Down</button>
     </div>
