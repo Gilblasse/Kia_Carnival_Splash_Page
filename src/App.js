3107d57a7d0183220a417e9959305e0abd45189c
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-09-05 18:15:55 +0000

    creating Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import React, { useState, useEffect } from "react";
 import './App.css';
-import Features from "./components/Features";
+import Features from './components/Features';
 
 function App() {
  return(
@@ -9,7 +9,7 @@ function App() {
     <div className='leftSide'>
       <h1 className='leftSideHeader'>Connectivity at every turn.</h1>
       <div className='vehicleFeaturesList'>
-        {/* <Features/> */}
+        {/* <Features /> */}
       </div>
       <button className='button'>up</button>
       <button className='button'>Down</button>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Features from './components/Features';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Features />
   </React.StrictMode>
 );
 
