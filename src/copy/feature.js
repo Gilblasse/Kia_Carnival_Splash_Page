17fd7c1ca4d9f031069445886434f0ec06b81eb4
JasminDe <jblasse95@gmail.com>
2023-09-05 19:51:45 -0400

    added dicriptions for features

---
+++ src/copy/feature.js
@@ -0,0 +1,7 @@
+export default {
+    feature1: `Start your vehicle remotely and set the cabin temperature before you even step inside.1`,
+    feature2: `Conveniently locate and get directions to your car on demand using the Kia Access app.2`,
+    feature3: `Kia Connect connects with Google Assistant™ & Amazon Alexa so you can use voice commands for features like remote start and climate control.3`,
+    feature4: `The available navigation system uses real-time and historical traffic data, as well as weather and speed limit info to predict the quickest route between two points.4`,
+    feature5: `If equipped, stay connected on the go and enjoy movies, games, social media, and more with the available Wi-Fi hotspot.5`
+}
