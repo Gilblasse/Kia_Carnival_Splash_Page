1eb4eccf8d0070538dd712ffe7877f5e8dc4dcc7
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-09-05 00:03:57 +0000

    adding h2 tag for leftSideHeader

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
  return(
   <div className='main-container'>
-    <div className='leftSide'></div>
+    <div className='leftSide'>
+      <h2 className='leftSideHeader'>'Connectivity at every turn.'</h2>
+    </div>
     <div className='rightSide'></div>
   </div>
  )
