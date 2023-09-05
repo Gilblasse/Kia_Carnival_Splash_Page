8f02f8fc92aa4ba21f2d9a67735217c4b5bc7d5f
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-09-05 00:11:47 +0000

    added vehicleFeaturesList div

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   <div className='main-container'>
     <div className='leftSide'>
       <h1 className='leftSideHeader'>Connectivity at every turn.</h1>
+      <div className='vehicleFeaturesList'></div>
     </div>
     <div className='rightSide'></div>
   </div>
