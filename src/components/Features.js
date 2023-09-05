954abf1a18f7e648c23b7dc1f8151432ab69ce8e
JasminDe <jblasse95@gmail.com>
2023-09-05 19:26:10 -0400

    created Features component

---
+++ src/components/Features.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Features({title, discription}) {
+  return (
+    <div>{title}</div>
+    <div>{discription}</div>
+  )
+}
+
+// each needs a title and discription
