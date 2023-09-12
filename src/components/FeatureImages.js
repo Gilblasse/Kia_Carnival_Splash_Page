87784f3a983f5e2d09f76d1cb57b5e167f4f16b7
JasminDe <jblasse95@gmail.com>
2023-09-12 17:40:30 -0400

    created FeatureImg Component

---
+++ src/components/FeatureImages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FeatureImages() {
+  return (
+   <div className='img_container'>
+        <img src="/images/imageComingSoon.jpg" alt=""/>
+   </div>
+  )
+}
