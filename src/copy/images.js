4510663bd8519a924ff4465c21ef2c501e9b5554
JasminDe <jblasse95@gmail.com>
2023-09-12 17:42:11 -0400

    created an images component

---
+++ src/copy/images.js
@@ -0,0 +1,7 @@
+export default {
+    image1: <img src="/images/Coming-Soon.png.png" alt=""/>,
+    image2: "/images/imageComingSoon.jpg",
+    // image3: `Kia Connect connects with Google Assistant™ & Amazon Alexa so you can use voice commands for features like remote start and climate control.3`,
+    // image4: `The available navigation system uses real-time and historical traffic data, as well as weather and speed limit info to predict the quickest route between two points.4`,
+    // image5: `If equipped, stay connected on the go and enjoy movies, games, social media, and more with the available Wi-Fi hotspot.5`
+}
