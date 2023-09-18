1557a08a13acd016b96ef49d8cae8f52d40477c3
JasminDe <jblasse95@gmail.com>
2023-09-18 19:11:08 -0400

    added change image capabilities

--- src/components/FeatureImages.js
+++ src/components/FeatureImages.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import images from '../constants/images'
 
-export default function FeatureImages() {
+export default function FeatureImages({x}) {
   return (
    <div className='img_container'>
-        <img src="/images/imageComingSoon.jpg" alt=""/>
+        <img src={images[x]} alt=""/>
    </div>
   )
 }
