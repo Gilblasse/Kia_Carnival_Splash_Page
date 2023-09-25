a3996bbd514e5ff5d6aca6764006460adbb29dcf
JasminDe <jblasse95@gmail.com>
2023-09-25 09:35:48 -0400

    added inactive and active stlyes to features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-export default function Features({title, discription}) {
+export default function Features({title, discription, active, onClick}) {
+  const inactiveStyle = {
+    opacity: 0.6
+  }
+
+  const style = active ? {
+    fontWeight: 'bold'
+  } : inactiveStyle
+
   return (
-    <div>
-      <h2>{title}</h2>
-      <div className='item_discription'>{discription}</div>
+    <div onClick={onClick}>
+      <h2 style={style}>{title}</h2>
+      <div style={style} className='item_discription'>{discription}</div>
     </div>
   )
 }
