98fe9d53463a14e2e89ef62194dce350a4c91a40
JasminDe <jblasse95@gmail.com>
2023-09-25 09:35:08 -0400

    change img keys

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,8 @@
 export default {
-    image1: "images/kia_carnival_2024_mep_asset-list_remote-start-with-climate-control-v2_XL.jpg",
-    image2: "/images/kia_carnival_2024_mep_asset-list_find-my-car-v2_XL.jpg",
-    image3: "/images/kia_carnival_2024_mep_asset-list_voice-assistance-v2_XL.jpg",
-    image4: "/images/kia_carnival_2024_mep_asset-list_connected-routing-v2_XL.jpg",
-    image5: "/images/kia_carnival_2024_mep_asset-list_wifi-hotspot-v2_XL.jpg",
+    feature1: "images/kia_carnival_2024_mep_asset-list_remote-start-with-climate-control-v2_XL.jpg",
+    feature2: "/images/kia_carnival_2024_mep_asset-list_find-my-car-v2_XL.jpg",
+    feature3: "/images/kia_carnival_2024_mep_asset-list_voice-assistance-v2_XL.jpg",
+    feature4: "/images/kia_carnival_2024_mep_asset-list_connected-routing-v2_XL.jpg",
+    feature5: "/images/kia_carnival_2024_mep_asset-list_wifi-hotspot-v2_XL.jpg",
+    feature6: '/images/featureComingSoon.jpg'
 }
