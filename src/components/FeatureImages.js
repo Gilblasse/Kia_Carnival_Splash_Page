f1a6bf8ba8de0ee711253a0878d310312ce04cf0
JasminDe <jblasse95@gmail.com>
2023-09-25 11:53:37 -0400

    removed console log

--- src/components/FeatureImages.js
+++ src/components/FeatureImages.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function FeatureImages({srcImg}) {
-  console.log({ srcImg })
   return (
    <div className='img_container'>
         <img src={srcImg} alt="feature image"/>
