7de04888fb1e7c17aa20ed56e58527158ba6d3d3
JasminDe <jblasse95@gmail.com>
2023-10-03 17:44:06 -0400

    removed progressbar functionalities

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,20 +1,25 @@
-import React from 'react'
 
-export default function Features({title, discription, active, onClick}) {
+
+export default function Features({title, discription, active, onClick, time}) {
+
+
   const inactiveStyle = {
     opacity: 0.6
   }
 
   const style = active ? {
-    fontWeight: 'bold'
+    opacity: 1
   } : inactiveStyle
 
   return (
-    <div onClick={onClick}>
-      <h2 style={style}>{title}</h2>
-      <div style={style} className='item_discription'>{discription}</div>
+    <div className='bar_and_discription' onClick={onClick}>
+      <div className='progress_bar_container' style={inactiveStyle}>
+      <div style={{width:'5px',height: `${time}px`}} className='progress_bar'></div>
+      </div>
+      <div >
+        <h2 style={style}>{title}</h2>
+        <div style={style} className='item_discription'>{discription}</div>
+      </div>
     </div>
   )
 }
-
-// each needs a title and discription
